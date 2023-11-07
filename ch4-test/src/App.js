af5869058a77c14a6e2a15acc68fc1f12067dc35
lhj257 <139347455+lhj257@users.noreply.github.com>
2023-11-07 16:11:46 +0900

    231107 7교시

---
+++ ch4-test/src/App.js
@@ -0,0 +1,28 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Join from "./component/Join";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        {/* 회원 가입 폼으로 작성할 컴포넌트 */}
+        <Join />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
