01739374d3ac888a1838a5168e3ebb0b757abc63
lhj257 <139347455+lhj257@users.noreply.github.com>
2023-11-13 15:52:14 +0900

    231113 6교시

--- chjoin-test/src/ch10_TodoTest/TodoList.js
+++ chjoin-test/src/ch10_TodoTest/TodoList.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useCallback } from "react";
 import styled from "styled-components";
 import TodoListItem from "./TodoListItem";
+
+//페이징 처리 해주는 가상의 리스트 불러오기
+import { List } from "react-virtualized";
 
 //전체 리스트 부분만 css 작업. TodoListCss
 const TodoListCss = styled.div`
@@ -19,8 +22,39 @@ const TodoListCss = styled.div`
   /* <TodoList todos={todos} onToggle={onToggle} /> */
 }
 const TodoList = ({ todos, onRemove, onToggle }) => {
+  // react-virtualized 이용해서, 페이징하기, 현재, 리스트 목록부분만의 사이즈 : 512X513(9개 기준)
+  //  하나의 높이를 대략 : 57px 정함(57*9=513)
+  // rowRender, 가상의 행을 나타내는 함수 정의하기.
+  const rowRender = useCallback(
+    ({ index, key, style }) => {
+      const todo = todos[index];
+      return (
+        <TodoListItem
+          todo={todo}
+          key={key}
+          onRemove={onRemove}
+          onToggle={onToggle}
+          styel={style}
+        />
+      );
+    },
+    [onRemove, onToggle, todos]
+  );
+
   return (
-    <TodoListCss>
+    // 기존의 리스트 -> 가상의 리스트로 교체후
+    // css 속성 추가하기
+    // <TodoListCss>
+    // 부모 리스트에서 props로 담아서, TodoListItem 전달하기.
+    <List
+      width={512}
+      height={513}
+      rowCount={todos.length}
+      rowHeight={57}
+      rowRenderer={rowRender}
+      list={todos}
+      style={{ outline: "none" }}
+    >
       {/* 부모에서 전달받은 데이터를 사용해보기 */}
       {todos.map((todo) => (
         //  TodoList 부모 컴포넌트에, 다시 , 자식 컴포넌트인 TodoListItem에게 props로 전달중
@@ -42,8 +76,10 @@ const TodoList = ({ todos, onRemove, onToggle }) => {
       <TodoListItem />
       <TodoListItem /> 
       자식에서, 더미 데이털 직접 만들어서 사용했다면*/}
-    </TodoListCss>
+    </List>
+    // </TodoListCss>
   );
 };
 
-export default TodoList;
+//추가하기.
+export default React.memo(TodoList);
